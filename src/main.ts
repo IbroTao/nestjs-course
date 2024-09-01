8653e55512d2984509678210242ccfe62db33ac4
Taofeek <ibrahimtaofeek005@gmail.com>
2024-09-01 20:35:52 +0100

    testing app module

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import {NestFactory} from "@nestjs/core"
 import {AppModule} from "./app.module"
 
-async function runTest() {
+async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
-runTest()
+bootstrap()
