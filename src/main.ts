684699e43321efc80f2e3c7cacd59d0db7158fb6
Taofeek <ibrahimtaofeek005@gmail.com>
2024-09-08 12:33:59 +0100

    guards, pipes and interfaces learnt

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import {NestFactory} from "@nestjs/core"
 import {AppModule} from "./app.module"
 
-async function bootstrap() {
+async function myApp() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
-bootstrap()
+myApp()
