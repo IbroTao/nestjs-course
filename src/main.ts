164fd4ab2fdbc50a08ccb8dcf1946230df07404a
Taofeek <ibrahimtaofeek005@gmail.com>
2024-09-22 01:12:28 +0100

    change function name in the main fiel

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import {NestFactory} from "@nestjs/core"
 import {AppModule} from "./app.module"
 
-async function bootstrap() {
+async function app() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
-bootstrap()
+app()
