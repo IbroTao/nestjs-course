f89b0f3b7536e7ce9c3cdf2dd3b87441e185c585
Taofeek <ibrahimtaofeek005@gmail.com>
2024-10-08 16:19:30 +0100

    index file name updated

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 import {NestFactory} from "@nestjs/core"
 import {AppModule} from "./app.module"
 
-async function app() {
+async function bootstarp() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
-app()
+bootstarp()
